03004c195bb6b34fe0df808fb99401afbbc41990
MuresanRazv <razvy9099@yahoo.com>
2024-11-27 18:36:52 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Muresan_Razvan_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options): base(options)
+        {
+        }
+
+        public DbSet<ExpenseDTO> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Muresan_Razvan_Lab5.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<ExpenseContext>(opt => opt.UseInMemoryDatabase("ExpenseList"));
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
